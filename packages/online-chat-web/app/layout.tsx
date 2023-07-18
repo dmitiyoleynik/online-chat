a28e9ac5a6a97aeedd7ea31e28807aec4fffdce1
Dmytro <dmytro.oliinyk@ardas.biz>
2023-07-18 19:15:04 +0300

    Add color mode toggle

--- packages/online-chat-web/app/layout.tsx
+++ packages/online-chat-web/app/layout.tsx
@@ -1,19 +1,48 @@
+'use client';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {};
+
+export const ColorModeContext = React.createContext({
+    toggleColorMode: () => {},
+});
 
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                },
+            }),
+        [mode],
+    );
+    const toggleColorMode = () => {
+        setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
+    };
+
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
+        <ColorModeContext.Provider
+            value={{
+                toggleColorMode,
+            }}
+        >
+            <ThemeProvider theme={theme}>
+                <html lang="en">
+                    <body className={inter.className}>{children}</body>
+                </html>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     );
 }
--- packages/online-chat-web/app/page.tsx
+++ packages/online-chat-web/app/page.tsx
@@ -1,102 +1,42 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+'use client';
+
+import { Box, IconButton, useTheme } from '@mui/material';
+
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import React from 'react';
+import { ColorModeContext } from './layout';
 
 export default function Home() {
+    const { toggleColorMode } = React.useContext(ColorModeContext);
+    const theme = useTheme();
+
     return (
-        <main className={styles.main}>
-            <div className={styles.description}>
-                <p>
-                    Get started by editing&nbsp;
-                    <code className={styles.code}>app/page.tsx</code>
-                </p>
-                <div>
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By{' '}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className={styles.vercelLogo}
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
-                </div>
-            </div>
-
-            <div className={styles.center}>
-                <Image
-                    className={styles.logo}
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-            <div className={styles.grid}>
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
+        <div>
+            <Box
+                sx={{
+                    display: 'flex',
+                    width: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    bgcolor: 'background.default',
+                    color: 'text.primary',
+                    borderRadius: 1,
+                    p: 3,
+                }}
+            >
+                <IconButton
+                    sx={{ ml: 1 }}
+                    onClick={toggleColorMode}
+                    color="inherit"
                 >
-                    <h2>
-                        Docs <span>-&gt;</span>
-                    </h2>
-                    <p>
-                        Find in-depth information about Next.js features and
-                        API.
-                    </p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Learn <span>-&gt;</span>
-                    </h2>
-                    <p>
-                        Learn about Next.js in an interactive course
-                        with&nbsp;quizzes!
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Templates <span>-&gt;</span>
-                    </h2>
-                    <p>Explore the Next.js 13 playground.</p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className={styles.card}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2>
-                        Deploy <span>-&gt;</span>
-                    </h2>
-                    <p>
-                        Instantly deploy your Next.js site to a shareable URL
-                        with Vercel.
-                    </p>
-                </a>
-            </div>
-        </main>
+                    {theme.palette.mode === 'dark' ? (
+                        <Brightness7Icon />
+                    ) : (
+                        <Brightness4Icon />
+                    )}
+                </IconButton>
+            </Box>
+        </div>
     );
 }
