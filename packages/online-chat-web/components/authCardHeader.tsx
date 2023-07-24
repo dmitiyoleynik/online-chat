303f6bb8691312e3d6e55cc5a2feb521e710274f
Dmytro <dmytro.oliinyk@ardas.biz>
2023-07-24 13:13:53 +0300

    moved card header to separate component

---
+++ packages/online-chat-web/components/authCardHeader.tsx
@@ -0,0 +1,24 @@
+import { Avatar, Stack, Typography, useTheme } from '@mui/material';
+interface Props {
+    title?: string;
+}
+const AuthCardHeader: React.FC<Props> = ({ title }) => {
+    const theme = useTheme();
+
+    return (
+        <Stack alignItems="center">
+            <Avatar
+                sx={{
+                    height: theme.spacing(10),
+                    width: theme.spacing(10),
+                    bgcolor: 'teal',
+                    color: 'white',
+                }}
+            />
+
+            {title && <Typography variant="h3">{title}</Typography>}
+        </Stack>
+    );
+};
+
+export default AuthCardHeader;
